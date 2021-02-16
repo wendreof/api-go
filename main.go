5627198ed505dbf909e7f833ca9c8e5e82440beb
wlf <wendreolf@gmail.com>
2021-02-16 01:04:59 -0300

    Testing POST with Request Bin

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"time"
+
+	"github.com/wendreof/hello-go/api/models"
 )
 
 func main() {
@@ -12,35 +16,47 @@ func main() {
 		Timeout: time.Second * 30,
 	}
 
-	response, err := client.Get("https://www.google.com.br")
+	user := models.User{}
+	user.ID = 1
+	user.Name = "@wendreof"
+
+	content2Sent, err := json.Marshal(user)
 	if err != nil {
-		fmt.Println("[main] Was not possible reach the google servers: ",
+		fmt.Println("[main] Error on convert user 2 json: ",
 			err.Error())
 		return
 	}
 
-	defer response.Body.Close()
+	// response, err := client.Get("https://www.google.com.br")
+	// if err != nil {
+	// 	fmt.Println("[main] Was not possible reach the google servers: ",
+	// 		err.Error())
+	// 	return
+	// }
 
-	if response.StatusCode == 200 {
-		body, err := ioutil.ReadAll(response.Body)
-		if err != nil {
-			fmt.Println("[main] Was not possible read the content body: ",
-				err.Error())
-			return
-		}
-		fmt.Println(string(body))
-	}
+	// defer response.Body.Close()
 
-	request, err := http.NewRequest("GET", "https://www.google.com.br", nil)
+	// if response.StatusCode == 200 {
+	// 	body, err := ioutil.ReadAll(response.Body)
+	// 	if err != nil {
+	// 		fmt.Println("[main] Was not possible read the content body: ",
+	// 			err.Error())
+	// 		return
+	// 	}
+	// 	fmt.Println(string(body))
+	// }
+
+	request, err := http.NewRequest("POST", "https://enjyqmzivj2fp.x.pipedream.net/", bytes.NewBuffer(content2Sent))
 	if err != nil {
-		fmt.Println("[main] Was not possible request it: ",
+		fmt.Println("[main] Was not possible send it 2 request bin: ",
 			err.Error())
 		return
 	}
-	request.SetBasicAuth("teste", "teste")
-	response, err = client.Do(request)
+	request.SetBasicAuth("fizz", "buzz")
+	request.Header.Set("content-type", "application/jsonb; charset=utf8")
+	response, err := client.Do(request)
 	if err != nil {
-		fmt.Println("[main] Was not possible reach the google servers: ",
+		fmt.Println("[main] Was not possible reach the request bin servers: ",
 			err.Error())
 		return
 	}
---
+++ models/user.go
@@ -0,0 +1,7 @@
+package models
+
+//User represents the user of the system
+type User struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
